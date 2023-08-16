e6327082df79e4edca9851cb413e3366da9f78c3
Luevano-0709 <122376656+Luevano-0709@users.noreply.github.com>
2023-08-16 11:21:19 -0700

    para las notificaciones

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
-import cors from "cors"
+import cors from "cors";
+import OneSignal from 'react-onesignal';
 
 import ph_Routes_Routes from "./routes/ph.routes.js";
 const app=express();
@@ -16,5 +17,20 @@ app.use(express.json())
 
 //rutas
 app.use("/api/ph",ph_Routes_Routes)
+
+function app() {
+    useEffect(() => {
+		OneSignal.init({
+			appId: "c6d9496d-9544-4779-98a2-00ca60571ed3"
+		});
+	}, []);
+
+    const onHandTag = (tag) => {
+        console.log('Tagging');
+        OneSignal.sendTag('tech', tag).then(() => {
+            console.log('Tagged');
+        });
+    }
+}
 
 export default app;
