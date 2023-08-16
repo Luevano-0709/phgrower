d63696c3270bdce5b45f72fae7f2a66c9181d3f8
Luevano-0709 <122376656+Luevano-0709@users.noreply.github.com>
2023-08-16 12:37:55 -0700

    Add files via upload

--- src/pages/Tabla.jsx
+++ src/pages/Tabla.jsx
@@ -5,22 +5,22 @@ export const Tabla = () => {
   return (
     <>
     <div className='cont2'>
-    <h2 style={{padding:"10px"}}>Sugerencias para el cuidado de tu ph: </h2>
+    <h1 style={{padding:"10px"}}>Sugerencias para el cuidado de tu ph: </h1>
 
-      <p style={{padding:"10px"}}>1- Siempre ten cuidado al nivel que se muestra en la pantalla. </p>
+      <h2 style={{padding:"10px"}}>1- Siempre ten cuidado al nivel que se muestra en la pantalla. </h2>
 
-      <p style={{padding:"10px"}}>2- El ph de cada planta varia dependiendo, como el arroz y el maiz tiene un nivel diferente de ph.</p>
+      <h2 style={{padding:"10px"}}>2- El ph de cada planta varia dependiendo, como el arroz y el maiz tiene un nivel diferente de ph.</h2>
 
-      <p style={{padding:"10px"}}>3- Ten en cuenta la fertilizasion que se utilizara en el cultivo.</p>
+      <h2 style={{padding:"10px"}}>3- Ten en cuenta la fertilizasion que se utilizara en el cultivo.</h2>
 
-      <p style={{padding:"10px"}}>4- El nivel de pH del suelo o del sustrato juega un papel importante en la elección del tipo de quelato adecuado: 
-                                  un quelato DTPA pierde parte de su efecto a pH 6.5, ten esto en consideración al elegir el tipo de quelato.</p>
+      <h2 style={{padding:"10px"}}>4- El nivel de pH del suelo o del sustrato juega un papel importante en la elección del tipo de quelato adecuado: 
+                                  un quelato DTPA pierde parte de su efecto a pH 6.5, ten esto en consideración al elegir el tipo de quelato.</h2>
 
-      <p style={{padding:"10px"}}>5 - Un nivel de pH incorrecto en el tanque de fertilizantes puede causar floculación y sedimentación de los fertilizantes 
-                                  y/o microelementos. Esto puede obstruir tu tanque de fertilizantes. </p>   
+      <h2 style={{padding:"10px"}}>5 - Un nivel de pH incorrecto en el tanque de fertilizantes puede causar floculación y sedimentación de los fertilizantes 
+                                  y/o microelementos. Esto puede obstruir tu tanque de fertilizantes. </h2>   
 
-      <center><p style={{padding:"10px"}}>6 - En caso, de que el nivel de ph este bajo o alto, equilibralo utilizando las diversas formas que existen como: 
-                                  Aporte de materia orgánica, Aplicar fertilizantes nitrogenados, etc</p></center>                       
+      <center><h2 style={{padding:"10px"}}>6 - En caso, de que el nivel de ph este bajo o alto, equilibralo utilizando las diversas formas que existen como: 
+                                  Aporte de materia orgánica, Aplicar fertilizantes nitrogenados, etc</h2></center>                       
     </div>
     </>
   )
